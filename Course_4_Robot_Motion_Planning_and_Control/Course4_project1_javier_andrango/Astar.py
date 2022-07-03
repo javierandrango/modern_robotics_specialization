be335700df1080ffe68f48c9a377800d3ec01e56
Javier Andrango <andrangofjavier@outlook.com>
2022-07-02 22:44:23 -0500

    Course4 Project week1 PEP8 code review

--- Course_4_Robot_Motion_Planning_and_Control/Course4_project1_javier_andrango/Astar.py
+++ Course_4_Robot_Motion_Planning_and_Control/Course4_project1_javier_andrango/Astar.py
@@ -1,13 +1,14 @@
 import numpy as np
 import math as mt
 
-#function to create a neighbor node list
-def neighbor_list(edges,current,CLOSED):
+
+# function to create a neighbor node list
+def neighbor_list(edges, current, CLOSED):
     '''Create a list of neighbor nodes.
     Neighbot nodes(connected nodes)that are not included
     in CLOSED list.
     Input:
-        - current: actual node. 
+        - current: actual node.
         - edges: ID1,ID2 of the nodes connected by
           the edge. Matrix of the edges.csv file.
         - CLOSED: list of nodes already explored.
@@ -16,14 +17,15 @@ def neighbor_list(edges,current,CLOSED):
           in CLOSED list.
     '''
     nbr_list = []
-    #searching for neighbor ID1 --> ID2
-    for i in edges[:,0:2]:
+    # searching for neighbor ID1 --> ID2
+    for i in edges[:, 0:2]:
         if (current == i[0])and(not(i[1]in CLOSED)):
             nbr_list.append(i[1])
-      
-    #searching for neighbor ID2 --> ID1
-    for i in edges[:,0:2]: 
-        if (current == i[1])and(not(i[0] in CLOSED))and(not(current in nbr_list)):
+
+    # searching for neighbor ID2 --> ID1
+    for i in edges[:, 0:2]:
+        if (current == i[1])and(not(i[0] in CLOSED))and (not(
+                                                        current in nbr_list)):
             nbr_list.append(i[0])
 
     nbr_list.sort()
@@ -31,7 +33,7 @@ def neighbor_list(edges,current,CLOSED):
 
 
 # Function to return the cost between nodes
-def cost_edges(edges,ID1,ID2):
+def cost_edges(edges, ID1, ID2):
     ''' Return the cost between nodes stored in the
     "edges" matrix.
     Search a specific cost value in the "edges" matrix
@@ -42,17 +44,17 @@ def cost_edges(edges,ID1,ID2):
           by the edge
     Return:
         - cost: the cost of traversing that edge (in
-         either direction)   
+          either direction)
     '''
-    ids = [ID1,ID2]
+    ids = [ID1, ID2]
     # sort the elements from higher to lower
     ids.sort(reverse=True)
     # search for the index and return a tuple single element(index)
-    index = np.where(edges[:,0] == ids[0])[0]
+    index = np.where(edges[:, 0] == ids[0])[0]
     # search the ID2 element and return the cost
     for i in index:
-        if(edges[i,1]==ids[1]):
-            cost = edges[i,2]
+        if(edges[i, 1] == ids[1]):
+            cost = edges[i, 2]
         else:
             continue
     return cost
@@ -62,7 +64,7 @@ def cost_edges(edges,ID1,ID2):
 def a_star(nodes, edges):
     '''A* search algorithm
     A* search pseudocode described in Modern Robotics book (Frank C. Park
-    & Kevin M.Lynch), Chapter 10. Motion Planning, page 367. 
+    & Kevin M.Lynch), Chapter 10. Motion Planning, page 367.
     input:
         - nodes: [ID,x,y,heuristic-cost-to-go]. Numpy array of
           data from node.csv file
@@ -74,58 +76,62 @@ def a_star(nodes, edges):
     '''
     # Initial values
     # goal,last node (last ID)
-    goal = nodes[-1,0]
+    goal = nodes[-1, 0]
 
     # heuristic cost to go from node file
     heuristic_ctg = {}
     # initial past cost for all nodes (node1=0, node2-N=inf)
-    past_cost={}
-    for i in range(1,len(nodes)+1):
-        heuristic_ctg[i] = nodes[i-1,-1]
+    past_cost = {}
+    for i in range(1, len(nodes) + 1):
+        heuristic_ctg[i] = nodes[i-1, -1]
         past_cost[i] = mt.inf
     past_cost[1] = 0
-    
+
     # add first node into OPEN list
     OPEN = []
-    OPEN.append(nodes[0,0])    
-    
+    OPEN.append(nodes[0, 0])
+
     # CLOSED list (empty at beginning)
     CLOSED = []
-    
-    # parent node(unknow value at beginning)
-    parent={}
 
-    # estimated total cost(unknow value at beginning)  
+    # parent node(unknow value at beginning)
+    parent = {}
+
+    # estimated total cost(unknow value at beginning)
     est_total_cost = {}
-        
+
     # while OPEN is not empty
     while(OPEN):
         current = OPEN[0]
         OPEN.remove(OPEN[0])
         CLOSED.append(current)
-        
+
         if (current == goal):
             print("SUCESS")
             return parent
             break
-        
-        for nbr in neighbor_list(edges,current,CLOSED):
-            tentative_past_cost = past_cost[current]+cost_edges(edges,current,nbr)
+
+        for nbr in neighbor_list(edges, current, CLOSED):
+            tentative_past_cost = past_cost[current] + \
+                                                cost_edges(edges, current, nbr)
             if(tentative_past_cost < past_cost[nbr]):
-                past_cost[nbr]= tentative_past_cost
-                parent[int(nbr)]= int(current)
-                est_total_cost[nbr]= past_cost[nbr]+ heuristic_ctg[nbr]
-                # sort estimate total cost dictionary(item from low to high value)
-                sort_est_total_cost = dict(sorted(est_total_cost.items(), key=lambda x: x[1]))
-                #add sorted node(i) sort_est_total_cost into OPEN list
-                #sort_est_total_cost = {node : estimation total cost}
+                past_cost[nbr] = tentative_past_cost
+                parent[int(nbr)] = int(current)
+                est_total_cost[nbr] = past_cost[nbr] + heuristic_ctg[nbr]
+                # sort estimate total cost dictionary(item from low to
+                # high value)
+                sort_est_total_cost = dict(
+                    sorted(est_total_cost.items(), key=lambda x: x[1]))
+                # add sorted node(i) sort_est_total_cost into OPEN list
+                # sort_est_total_cost = {node : estimation total cost}
                 for i in sort_est_total_cost:
                     if(not(i in OPEN)) and (not(i in CLOSED)):
                         OPEN.append(i)
     return "FAILURE"
 
+
 # function to calculate a short path
-def short_path(nodes,parent_dict):
+def short_path(nodes, parent_dict):
     ''' Short path of a reconstructed path retrieved
         from A* search.
         Returns a list representing the short path (nodes
@@ -137,12 +143,12 @@ def short_path(nodes,parent_dict):
         - parent_dict: reconstructed path retrieved from A*
           search
     output:
-        - short_path: resulting short path (nodes from low to high)          
+        - short_path: resulting short path (nodes from low to high)
     '''
     short_path = []
-    iteration_node = int(nodes[-1,0])
-    first_node = int(nodes[0,0])
-    while(iteration_node>first_node):
+    iteration_node = int(nodes[-1, 0])
+    first_node = int(nodes[0, 0])
+    while(iteration_node > first_node):
         short_path.append(iteration_node)
         iteration_node = parent_dict[iteration_node]
         if(iteration_node == first_node):
@@ -151,4 +157,3 @@ def short_path(nodes,parent_dict):
             continue
     short_path.reverse()
     return short_path
-
--- Course_4_Robot_Motion_Planning_and_Control/Course4_project1_javier_andrango/main.py
+++ Course_4_Robot_Motion_Planning_and_Control/Course4_project1_javier_andrango/main.py
@@ -1,9 +1,10 @@
 import numpy as np
 import csv
-#python file with A* algorithm and useful functions
+# python file with A* algorithm and useful functions
 from Astar import *
 
-#CSV reader function
+
+# CSV reader function
 def csv_reader(file_path):
     '''CSV files reader function.
     Only valid for files with a specific format
@@ -18,34 +19,28 @@ def csv_reader(file_path):
         reader = csv.reader(file)
         data = []
         for row in reader:
-            if row[0][0]!='#':
+            if row[0][0] != '#':
                 data.append(row)
     node = np.array(data).astype("float")
     return node
 
 # data reader
-#- ID: unique integer ID number of the node (1 through N)
-#- x,y: location of the node in the plane
-#- heuristic-cost-to-go: is an optimistic estimate of the path
-#  length from that node to the goal node
-#- ID1, ID2: are the IDs of the nodes connected by the edge
-#- cost: the cost of traversing that edge (in either direction)
+# - ID: unique integer ID number of the node (1 through N)
+# - x,y: location of the node in the plane
+# - heuristic-cost-to-go: is an optimistic estimate of the path
+#   length from that node to the goal node
+# - ID1, ID2: are the IDs of the nodes connected by the edge
+# - cost: the cost of traversing that edge (in either direction)
 nodes_file = 'results/nodes.csv'  # ID,x,y,heuristic-cost-to-go
 edges_file = 'results/edges.csv'  # ID1,ID2,cost
 nodes = np.array(csv_reader(nodes_file))
 edges = np.array(csv_reader(edges_file))
 
-#functions fetched from Astart.py
-parent_dict= a_star(nodes,edges)
-path = short_path(nodes,parent_dict)
+# functions fetched from Astart.py
+parent_dict = a_star(nodes, edges)
+path = short_path(nodes, parent_dict)
 print(path)
 
-#save path in a CSV file
-np.savetxt("results/path.csv",[path], delimiter=',', fmt='%i')
-        
-    
-    
-
-
-
+# save path in a CSV file
+np.savetxt("results/path.csv", [path], delimiter=',', fmt='%i')
 
